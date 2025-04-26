4b254258b6b9836cee546a3c292feeaee486997b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 17:15:30 -0300

    attt

--- luasrc/llm.lua
+++ luasrc/llm.lua
@@ -32,6 +32,28 @@ function newLLM(permissions)
         end
         llm.add_function("read", "read a file and return the content of the file",args,callback)
     end
+    if permissions.write then
+        local args = {
+            {name = "file",description="the file name", type = "string", required = true},
+            {name = "content",description="the content to write", type = "string", required = true},
+        }
+        local callback = function(args)
+            while true do 
+                local permit = input("Do you want to write the file? "..args.file.."(y/n): ")
+                if permit == "y" then
+                    print(YELLOW.."Writing file: "..args.file..RESET)
+                    break
+                elseif permit == "n" then
+                    print(RED.."Aborting write file: "..args.file..RESET)
+                    return "User aborted"
+                else
+                    print(RED.."Invalid input, please enter y or n"..RESET)
+                end
+            end 
+            return dtw.write_file(args.file, args.content)
+        end
+        llm.add_function("write", "write a file and return the content of the file",args,callback)
+    end
 
   return llm
 
--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ llm = newLLM({
     list = true
 })
 
-llm.add_user_prompt("read the content of README.md file and  make a summary of the content")
+llm.add_user_prompt("create a quck explanation of flask in README.md")
 
 response = llm.generate()
 print("Response: " .. response)
