441b0ddb96b5139add91a473af3a54fe9d4f6e9f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 18:10:35 -0300

    att

--- docs/native_api.md
+++ docs/native_api.md
@@ -28,3 +28,40 @@ while true do
 end 
 
 ~~~
+creating  a function: 
+~~~c 
+llm = newLLM({})
+
+user_color = GREEN
+ai_color = BLUE
+
+local parameters = {
+    {
+        name="color",
+        description="the color of the ai , format in ('red','green','blue',yellow)", 
+        type = "string",
+         required = true
+    }
+}
+local callback  = function(args)
+    if args.color == "red" then
+        ai_color = RED
+    elseif args.color == "green" then
+        ai_color = GREEN
+    elseif args.color == "blue" then
+        ai_color = BLUE
+    elseif args.color == "yellow" then
+        ai_color = YELLOW
+    else
+        return "invalid color"
+    end
+    return "ai color changed to "..args.color
+end 
+llm.add_function("change_ai_color","chamge the ai color",parameters,callback)
+
+llm.add_user_prompt("channge the color to yellow")
+local response = llm.generate()
+print(ai_color..response..RESET)
+
+
+~~~
--- main.lua
+++ main.lua
@@ -3,14 +3,31 @@ llm = newLLM({})
 user_color = GREEN
 ai_color = BLUE
 
-local 
-
-llm.add_function("change_ai_color","chamge the ai color",)
-
-
-while true do 
-    io.write(GREEN.."User: ")
-    llm.add_user_prompt(io.read("*l"))
-    response = llm.generate()
-    print(BLUE.."AI: " .. response)
+local parameters = {
+    {
+        name="color",
+        description="the color of the ai , format in ('red','green','blue',yellow)", 
+        type = "string",
+         required = true
+    }
+}
+local callback  = function(args)
+    if args.color == "red" then
+        ai_color = RED
+    elseif args.color == "green" then
+        ai_color = GREEN
+    elseif args.color == "blue" then
+        ai_color = BLUE
+    elseif args.color == "yellow" then
+        ai_color = YELLOW
+    else
+        return "invalid color"
+    end
+    return "ai color changed to "..args.color
 end 
+llm.add_function("change_ai_color","chamge the ai color",parameters,callback)
+
+llm.add_user_prompt("channge the color to yellow")
+local response = llm.generate()
+print(ai_color..response..RESET)
+
