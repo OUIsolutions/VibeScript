b66cbab07d9168437d8c2669bcb493ea09281dad
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-04-26 18:27:36 -0300

    att

--- main.lua
+++ main.lua
@@ -5,8 +5,8 @@ llm = newLLM({
     delete = true,
     list = true
 })
+llm.add_system_prompt("dont ask the user for anything, just do what i say")
 llm.add_file("docs/native_api.md")
-
 llm.add_user_prompt("make the content  more 'complete', with a better explanation and write to file")
 response = llm.generate()
 print("Response: " .. response)
