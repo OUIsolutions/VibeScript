67965bc9d62c17d5c0ddc6c838e4d07283f123f3
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-09 00:15:03 -0300

    attt

--- csrc/start.c
+++ csrc/start.c
@@ -1,6 +1,8 @@
 
 
 #include "imports/imports.fdefine.h"
-int lua_open_private_vibe_script(LuaCEmbed *args){
 
+int vibescript_start(lua_State *state){
+    LuaCEmbed * l  = newLuaCEmbedLib(state);
+    return LuaCembed_perform(l);
 }
