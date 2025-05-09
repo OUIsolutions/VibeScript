522da71dc3ebc24dda3f9ffe5d13b0ea8fdb9f14
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-08 23:24:48 -0300

    att

--- build/types.lua
+++
@@ -1,8 +0,0 @@
-function Embed_types(project)
-    local types = ""
-    local types_files = darwin.dtw.list_files_recursively("types", true)
-    for i = 1, #types_files do
-        types = types .. "\n" .. darwin.dtw.load_file(types_files[i])
-    end
-    project.embed_global("PRIVATE_DARWIN_TYPES", types)
-end
