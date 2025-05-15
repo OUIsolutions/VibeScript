637e160de373d66ac9daa0cfe2981b078af9e543
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-15 00:07:15 -0300

    att

--- csrc/start.c
+++ csrc/start.c
@@ -8,7 +8,13 @@ int vibescript_start(lua_State *state){
     LuaCEmbed_load_lib_from_c(l,load_lua_fluid_json,"json");
 
     #ifdef _WIN32
-       
+    LuaCEmbed_set_global_string(l,"os_name","windows");
+    #elif __APPLE__
+    LuaCEmbed_set_global_string(l,"os_name","mac");
+    #elif __linux__
+    LuaCEmbed_set_global_string(l,"os_name","linux"); 
     #endif
+
+    
     return LuaCembed_perform(l);
 }
