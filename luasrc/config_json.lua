93758af18661320ff231584446114e2332eae87e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-15 01:59:37 -0300

    att

--- luasrc/config_json.lua
+++ luasrc/config_json.lua
@@ -1,9 +1,11 @@
 
 private_vibescript.get_config_path = function()
     local config_path = argv.get_flag_arg_by_index({ "config" },0)
+    
     if config_path then
         return config_path
     end
+
     local name = cvibescript.get_config_name()
     if os_name == "windows" then
         return os.getenv("APPDATA") .. "/"..name
--- luasrc/main.lua
+++ luasrc/main.lua
@@ -1,7 +1,7 @@
 
 
-private_vibescript.main()
+private_vibescript.main = function()
     local config_path = private_vibescript.get_config_path()
-  
+    print("Config path: " .. config_path)
     
 end 
