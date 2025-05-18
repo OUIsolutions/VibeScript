b62c40707d4b58418bec7d3beae1d72dcaee5f65
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-18 01:00:36 -0300

    att

--- luasrc/constants/argv.lua
+++ luasrc/constants/argv.lua
@@ -1,15 +1,10 @@
 
 --- flags
 private_vibescript.CONFIG = "config"
-private_vibescript.CATEGORY = "category"
-private_vibescript.NAME = "name"
 private_vibescript.FILE = "file"
 --- actions
 private_vibescript.LIST_MODELS = "list_model"
 private_vibescript.SET_MODEL_AS_DEFAULT = "set_model_as_default"
 private_vibescript.CONFIGURE_MODEL = "configure_model"
 private_vibescript.RESET_CONFIG = "reset_config"
-private_vibescript.ADD_CATEGORY = "add_category"
-private_vibescript.REMOVE_CATEGORY = "remove_category"
 private_vibescript.ADD_SCRIPT = "add_script"
-private_vibescript.LIST_CATEGORYS = "list_categorys"
