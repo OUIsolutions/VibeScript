915a3efa5a74c442bb4ee75cfe26ee651692a00b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-21 22:14:13 -0300

    attat

--- teste.lua
+++ teste.lua
@@ -1,5 +1,10 @@
-
---set_prop("teste",{name="mateus"})
-
-name = get_prop("teste").name
-print(name)
+llm = newLLM({
+    read = true,
+    write = true,
+    execute = true,
+    delete = true,
+    list = true
+})
+llm.add_user_prompt("você ta funcionando ai parsa ? ")
+response = llm.generate()
+print("Response: " .. response)
