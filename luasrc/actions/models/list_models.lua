2cffec6fb56e43e0f2c36357d995229cf7247403
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-23 22:36:30 -0300

    Add default model indicator in list_models output

--- luasrc/actions/models/list_models.lua
+++ luasrc/actions/models/list_models.lua
@@ -2,6 +2,9 @@
 private_vibescript.list_models = function(config_json)
     for i = 1, #config_json.models do
         local model = config_json.models[i]
+        if model.name == config_json.default_model then
+            print("Default: True")
+        end
         print("Model Name: " .. model.name)
         print("URL: " .. model.url)
         print("-------------------------")
