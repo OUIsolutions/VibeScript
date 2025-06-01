8e55340fc213383941757515ea9114d011b33f6b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-01 01:49:59 -0300

    reverter

--- luasrc/main.lua
+++ luasrc/main.lua
@@ -41,27 +41,26 @@ private_vibescript.internal_main = function()
 
      --if it gets here , it will make the normal operation, which is to interpret the first arg
      local script_name = action
-     
-     path = script_name
-     local found_path = false
+     filename = script_name
+     local found_filename = false
 
      for i = 1 , #config_json.scripts do
           if config_json.scripts[i].name == script_name then
-               path = config_json.scripts[i].file
-               found_path = true
+               filename = config_json.scripts[i].file
+               found_filename = true
                break
           end
      end
      
-     if not found_path then 
-          path = dtw.get_absolute_path(script_name)          
+     if not found_filename then 
+          filename = dtw.get_absolute_path(script_name)          
      end
-     if not path then
+     if not filename then
           error("File ("..script_name..") does not exist",0)
      end
 
-     script_dir_name = dtw.newPath(path).get_dir()
-     dofile(path)
+     script_dir_name = dtw.newPath(filename).get_dir()
+     dofile(filename)
 
 end 
 private_vibescript.main = function()
