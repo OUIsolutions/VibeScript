6a060d096376512776b8b322d3934c3aab713731
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-01 01:56:54 -0300

    add relative dir load

--- luasrc/relative_load.lua
+++ luasrc/relative_load.lua
@@ -14,3 +14,16 @@ function relative_load(path)
     script_dir_name = old_script_dir_name
     filename = old_filename
 end 
+
+function relative_dir_load(path)
+    local target_dir = dtw.concat_path(script_dir_name, path)
+    local files = dtw.list_files_recursively(target_dir, true)
+    for i=1,#files do 
+        local current = dtw.get_absolute_path(files[i])
+        
+        -- avoid recursive loading of the same file
+        if current ~= filename  then 
+            dofile(current)
+        end 
+    end
+end 
