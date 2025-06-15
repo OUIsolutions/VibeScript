90acb87160bb5ea4c7e6b331ecdecad6a1bf01dc
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-15 06:34:26 -0300

    builded

--- build/build/rpm_static_build.lua
+++ build/build/rpm_static_build.lua
@@ -60,7 +60,7 @@ chmod +x %{buildroot}/usr/local/bin/PROJECT_NAME
     darwin.dtw.write_file(".cache/rpm_static_build/SPECS/project.spec", formmatted_rpm)
     os.execute("mkdir -p .cache/rpm_static_build/RPMS")
 
-    local image = darwin.ship.create_machine("fedora:41")
+    local image = darwin.ship.create_machine("almalinux:latest")
     image.provider = CONTANIZER
     image.add_comptime_command("dnf install rpm-build rpmdevtools -y")
     image.add_comptime_command("dnf install gcc -y")
