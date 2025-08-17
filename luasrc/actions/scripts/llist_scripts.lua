c80226d2764d73288be31f31dccfbd5e71579c98
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-16 22:39:37 -0300

    att

--- luasrc/actions/scripts/llist_scripts.lua
+++ luasrc/actions/scripts/llist_scripts.lua
@@ -4,16 +4,17 @@ private_vibescript.list_scripts = function(config_json)
     
     for i =1 , #config_json.scripts do
         local current_script = config_json.scripts[i]
-        
-        if start_filtrage then 
+        local print_script = false         
+        if start_filtrage then
             if dtw.starts_with(current_script.name, start_filtrage) then
-                print(current_script.name)
+                print_script = true
             end
         end 
-
-        if not start_filtrage then 
-            print(current_script.name)
+        if not start_filtrage then
+            print_script = true 
         end     
-        
+        if print_script then 
+            print(i..":"..current_script.name)
+        end 
     end
 end
