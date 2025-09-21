3c67228c796e4959e51e16ae9046b8bc9ac34639
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2025-09-21 07:43:58 -0300

    att

--- luasrc/main.lua
+++ luasrc/main.lua
@@ -71,45 +71,6 @@ private_vibescript.internal_main = function()
           return private_vibescript.set_model_as_default()
      end
 
-     --if it gets here , it will make the normal operation, which is to interpret the first arg
-     local script_name = action
-     filename = script_name
-     local found_filename = false
-     local scripts = get_prop("scripts", {})
-     local name_num = tonumber(script_name)
-     for i = 1 , #scripts do
-          if scripts[i].name == script_name then
-               filename = scripts[i].file
-               found_filename = true
-               break
-          end
-          if name_num == i then
-               filename = scripts[i].file
-               found_filename = true
-               break
-          end
-
-     end
-
-     if not found_filename then 
-          filename = dtw.get_absolute_path(script_name)          
-     end
-     if not filename then
-
-          local ok, requisition = pcall(luabear.fetch,{url=script_name})
-          if ok then 
-                 local ok, code  = pcall(requisition.read_body)
-               if ok then
-                    load(code)()
-                    return
-               end
-          end 
-          error("File ("..script_name..") does not exist",0)
-     end
-
-     script_dir_name = dtw.newPath(filename).get_dir()
-     dofile(filename)
-
 end 
 
 private_vibescript.main = function()
---
+++ luasrc/module_load.lua
@@ -0,0 +1,42 @@
+
+private_vibescript.module_load = function(script_name,load_as_module)
+
+     filename = script_name
+     local found_filename = false
+     local scripts = get_prop("scripts", {})
+     local name_num = tonumber(script_name)
+     for i = 1 , #scripts do
+          if scripts[i].name == script_name then
+               filename = scripts[i].file
+               found_filename = true
+               break
+          end
+          if name_num == i then
+               filename = scripts[i].file
+               found_filename = true
+               break
+          end
+
+     end
+
+     if not found_filename then 
+          filename = dtw.get_absolute_path(script_name)          
+     end
+     if not filename then
+
+          local ok, requisition = pcall(luabear.fetch,{url=script_name})
+          if ok then 
+                 local ok, code  = pcall(requisition.read_body)
+               if ok then
+                    load(code)()
+                    return
+               end
+          end 
+          error("File ("..script_name..") does not exist",0)
+     end
+
+     script_dir_name = dtw.newPath(filename).get_dir()
+     is_module = load_as_module or false
+     dofile(filename)
+
+end 
