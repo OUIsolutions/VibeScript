5038a0357c50a1d9a2b3818b483632284d8917aa
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-02 19:22:54 -0300

    att

--- csrc/start.c
+++ csrc/start.c
@@ -18,12 +18,13 @@ int vibescript_start(lua_State *state){
     #elif __linux__
         LuaCEmbed_set_global_string(l,"os_name","linux"); 
     #endif
+    LuaCEmbed_load_lib_from_c(l,luaopen_CandangoEngine_start_point,"candango");
 
     LuaCEmbed_load_lib_from_c(l,load_luaDoTheWorld,"dtw");
     LuaCEmbed_load_lib_from_c(l,load_lua_fluid_json,"json");
     LuaCEmbed_load_lib_from_c(l,serjao_berranteiro_start_point,"serjao");
     LuaCEmbed_load_lib_from_c(l,load_lua_bear,"luabear");
-    LuaCEmbed_load_lib_from_c(l,luaopen_CandangoEngine_start_point,"candango");
+
     LuaCEmbed_add_callback(l,"newRawLLM",private_new_raw_llm);
     LuaCEmbed_add_callback(l,"save_encrypted_prop",private_save_encrypted_data);
     LuaCEmbed_add_callback(l,"get_encrypted_prop",private_get_encrypted_data);
