d4501f569080f7490fe31a0bbb1e984199754078
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-03 12:49:00 -0300

    att

--- devops/build_and_release.lua
+++ devops/build_and_release.lua
@@ -1,9 +1,7 @@
 
 load_global_module("shipyard")
-
-
 dtw.remove_any("dependencies")
 dtw.remove_any("release")
 os.execute("darwin install")
 os.execute("darwin run_blueprint build/ --mode folder amalgamation_build alpine_static_build windowsi32_build windows64_build rpm_static_build debian_static_build --contanizer podman ")
-SHIPYARD_API.generate_release_from_json("shipyard devops/release.json")
+SHIPYARD_API.generate_release_from_json("devops/release.json")
