c6d1bfa78bef9db76960d4c7ac80e33f481b5d87
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-29 16:39:31 -0300

    att

--- luasrc/patch.lua
+++ luasrc/patch.lua
@@ -1,3 +1,14 @@
+
+private_vibescript.remove_itens_of_list = function (itens,itens_to_exclude)
+    local filtered_itens = {}
+    for i=1,#itens do
+        local item = itens[i]
+        local exclude_item = false
+        
+   
+    end
+end
+
 private_vibescript.configure_patch = function ()
     function ApplyPatch(patch)
 
@@ -29,6 +40,13 @@ private_vibescript.configure_patch = function ()
         end 
         local git_pull_command = "cd "..patch_folder.." && git pull"
         os.execute(git_pull_command)
+
+        local itens_to_copy = dtw.list_files_recursively(patch_folder.."/"..patch.src)
+
+        local internal_excudes = {
+            ".git/*"
+        }
+
 
     end
 end
