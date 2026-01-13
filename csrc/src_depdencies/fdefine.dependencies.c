3a06b62c4f3b9f6d257af0d862c610cfacbe3155
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-13 05:33:45 -0300

    att

--- csrc/src_depdencies/fdefine.dependencies.c
+++ csrc/src_depdencies/fdefine.dependencies.c
@@ -6,7 +6,7 @@
 #define error LUA_CEMBED_ERROR
 #include "../../dependencies/LuaCEmbed.c"
 #undef error
-#include "../../dependencies/doTheWorld.c"
+#include "../../dependencies/doTheWorldOne.c"
 #include "../../dependencies/BearHttpsClient.c"
 #include "../../dependencies/SDK_OpenAI.c"
 #include "../../dependencies/CWebStudioOne.c"
