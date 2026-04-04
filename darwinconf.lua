b80383f210c4f5510e5cb659b28bf87d3edc297f
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-04 15:42:02 -0300

    at

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,5 +1,5 @@
 PROJECT_NAME = "vibescript"
-CONTANIZER   =  darwin.argv.get_flag_arg_by_index({ "contanizer", }, 1,"docker" ) 
+CONTANIZER   =  darwin.argv.get_flag_arg_by_index({ "contanizer", }, 1,"podman" ) 
 VERSION      = "0.0.1"
 LICENSE      = "MIT"
 URL          = "https://github.com/OUIsolutions/VibeScript"
